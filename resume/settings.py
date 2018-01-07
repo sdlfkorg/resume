fddb8579021c6f8ab892eb97e75e842c8d99ddd8
sdlfkorg <sdlfkorg@gmail.com>
2018-01-07 20:24:46 +0800

    add heroku settings

--- resume/settings.py
+++ resume/settings.py
@@ -127,3 +127,22 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# internationalization settings
+USE_I18N = True
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+
+
+
+# heroku settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+DEBUG = False
+try:
+    from .local_settings import * 
+except ImportError:
+    pass
+
